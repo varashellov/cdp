20f9bebc42f1e4519e59637f10a4471563d46693
Mathias Fredriksson <mafredri@gmail.com>
2018-01-17 18:41:40 +0200

    session: Test edge cases, increases coverage

--- session/session_test.go
+++ session/session_test.go
@@ -45,86 +45,144 @@ func TestClient(t *testing.T) {
 
 	c := cdp.NewClient(conn)
 
-	sc, err := session.NewClient(c)
-	if err != nil {
-		t.Error(err)
-	}
-	defer sc.Close()
+	func() {
+		sc, err := session.NewClient(c)
+		if err != nil {
+			t.Error(err)
+		}
+		defer sc.Close()
 
-	createReply, err := c.Target.CreateTarget(ctx,
+		newPage := newTestTarget(t, ctx, c)
+		// Close later.
+		// defer newPage.Close()
+
+		// Test session usage.
+		pageConn, err := sc.Dial(ctx, newPage.ID)
+		if err != nil {
+			t.Error(err)
+		}
+		defer pageConn.Close()
+
+		pageC := cdp.NewClient(pageConn)
+
+		err = pageC.Page.Enable(ctx)
+		if err != nil {
+			t.Error(err)
+		}
+		fired, err := pageC.Page.DOMContentEventFired(ctx)
+		if err != nil {
+			t.Error(err)
+		}
+		defer fired.Close()
+
+		// TODO(maf): Use testdata / sample HTML for test.
+		_, err = pageC.Page.Navigate(ctx,
+			page.NewNavigateArgs("https://www.google.com"))
+		if err != nil {
+			t.Error(err)
+		}
+
+		_, err = fired.Recv()
+		if err != nil {
+			t.Error(err)
+		}
+
+		eval, err := pageC.Runtime.Evaluate(ctx, runtime.NewEvaluateArgs(`document.title`))
+		if err != nil {
+			t.Error(err)
+		}
+
+		var title string
+		err = json.Unmarshal(eval.Result.Value, &title)
+		if err != nil {
+			t.Error(err)
+		}
+
+		if !strings.Contains(title, "Google") {
+			t.Error("bad title:", title)
+		}
+
+		// Close the page, this should close pageConn.
+		newPage.Close()
+		select {
+		case <-pageConn.Context().Done():
+		case <-ctx.Done():
+			t.Error("timed out waiting for session to close")
+		}
+	}()
+
+	func() {
+		// Test connection closure, should close session client.
+		sc, err := session.NewClient(c)
+		if err != nil {
+			t.Error(err)
+		}
+		defer sc.Close()
+
+		// Test Dial after close.
+		newPage := newTestTarget(t, ctx, c)
+		defer newPage.Close()
+
+		_, err = sc.Dial(ctx, newPage.ID) // Closed by sc.Close().
+		if err != nil {
+			t.Error(err)
+		}
+
+		sc.Close()
+		_, err = sc.Dial(ctx, newPage.ID)
+		if err == nil {
+			t.Error("Dial: expected error after Close, got nil")
+		}
+	}()
+
+	func() {
+		// Test connection closure, should close session client.
+		sc, err := session.NewClient(c)
+		if err != nil {
+			t.Error(err)
+		}
+		defer sc.Close()
+
+		conn.Close()
+		time.Sleep(10 * time.Millisecond) // Give time for context propagation.
+		// TODO(mafredri): Confirm session client was closed.
+	}()
+
+	func() {
+		// Test creating a new client with a closed connection.
+		_, err := session.NewClient(c)
+		if err == nil {
+			t.Error("NewClient: rpcc.Conn is closed, expected error, got nil ")
+		}
+	}()
+}
+
+type testTarget struct {
+	t   *testing.T
+	ctx context.Context
+	c   *cdp.Client
+	ID  target.ID
+}
+
+func (t *testTarget) Close() {
+	reply, err := t.c.Target.CloseTarget(t.ctx,
+		target.NewCloseTargetArgs(t.ID))
+	if err != nil {
+		t.t.Error(err)
+	}
+	if !reply.Success {
+		t.t.Error("close target failed")
+	}
+}
+
+func newTestTarget(t *testing.T, ctx context.Context, c *cdp.Client) testTarget {
+	reply, err := c.Target.CreateTarget(ctx,
 		target.NewCreateTargetArgs("about:blank"))
 	if err != nil {
 		t.Error(err)
 	}
 
-	// Increase test coverage, slightly...
-	unusedConn, err := sc.Dial(ctx, createReply.TargetID)
-	if err != nil {
-		t.Error(err)
-	}
-	_ = unusedConn // Will be closed via sc.Close().
-
-	pageConn, err := sc.Dial(ctx, createReply.TargetID)
-	if err != nil {
-		t.Error(err)
-	}
-	defer pageConn.Close()
-
-	pageC := cdp.NewClient(pageConn)
-
-	err = pageC.Page.Enable(ctx)
-	if err != nil {
-		t.Error(err)
-	}
-	fired, err := pageC.Page.DOMContentEventFired(ctx)
-	if err != nil {
-		t.Error(err)
-	}
-	defer fired.Close()
-
-	// TODO(maf): Use testdata / sample HTML for test.
-	_, err = pageC.Page.Navigate(ctx,
-		page.NewNavigateArgs("https://www.google.com"))
-	if err != nil {
-		t.Error(err)
-	}
-
-	_, err = fired.Recv()
-	if err != nil {
-		t.Error(err)
-	}
-
-	eval, err := pageC.Runtime.Evaluate(ctx, runtime.NewEvaluateArgs(`document.title`))
-	if err != nil {
-		t.Error(err)
-	}
-
-	var title string
-	err = json.Unmarshal(eval.Result.Value, &title)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if !strings.Contains(title, "Google") {
-		t.Error("bad title:", title)
-	}
-
-	closeReply, err := c.Target.CloseTarget(ctx,
-		target.NewCloseTargetArgs(createReply.TargetID))
-	_ = closeReply
-	if err != nil {
-		t.Error(err)
-	}
-	if !closeReply.Success {
-		t.Error("close target failed")
-	}
-
-	// CloseTarget should cause pageConn to close.
-	select {
-	case <-pageConn.Context().Done():
-	case <-ctx.Done():
-		t.Error("timed out waiting for session to close")
-	}
+	return testTarget{t: t, ctx: ctx, c: c, ID: reply.TargetID}
 }
 
 var (
